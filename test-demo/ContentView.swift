4ff700a23805ad78ef489166c852088b550bd0c4
chris0512 <kyang3200@gmail.com>
2024-04-05 19:48:05 +0800

    Initial Commit

---
+++ test-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test-demo
+//
+//  Created by Chris on 2024/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test-demo/test_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_demoApp.swift
+//  test-demo
+//
+//  Created by Chris on 2024/4/2.
+//
+
+import SwiftUI
+
+@main
+struct test_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MagnificationGestureBootcamp()
+        }
+    }
+}
